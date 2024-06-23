b16fbb8c5b2901d00eec8091b86dc52ec4988007
Nicolas Pieter <nicolaspieter@yahoo.com>
2024-06-23 14:21:02 +0000

    Setup router

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import App from '~/pages/App';
+
+const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
+export default router;
